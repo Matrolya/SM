55f844a03cf20f5514b1bc1a159979793a8b1c94
Matrolya <olga.luckina@gmail.com>
2021-04-02 14:58:02 +0300

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,11 @@
+//slider===============================================
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+  });
+});
+
+//year==================================================
+$(document).ready(function () {
+  $("#year").html(" 2021");
+});
